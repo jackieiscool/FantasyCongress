6cf96dbedc5ab822a422bf16b3b1054498e0a8bb
dbc-apprentice <apprentice@devbootcamp.com>
2012-08-11 20:51:19 -0700

    Second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FantasyCongress::Application.config.secret_token = '4a224cb000ff1dc912461af0c92ad39120bd62e0b55874d977068619f2946ebe797c28cd64a2bff8032cc67f163f7fdc68f99368e0dc024945b84b4bfddeb82a'
