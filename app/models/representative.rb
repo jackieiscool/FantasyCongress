fbe20ca1e1a98392dcb558cd1017eeb5e2f68306
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-11-24 12:33:28 -0800

    teams table working

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -15,8 +15,8 @@
 
 class Representative < ActiveRecord::Base
   attr_accessible :caucus_id, :firstname, :lastname, :bioguide_id, :party, :state
-  has_many :teams, :through => :positions
   has_many :positions
+  has_many :teams, :through => :positions
 
   def name
     "#{self.firstname} #{self.lastname}" 
